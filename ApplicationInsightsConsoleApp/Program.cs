818990b2e26017f233e626888563679e4471a0a8
jke94 <jcarracedo.dev@gmail.com>
2023-05-06 10:37:59 +0200

    Base code.

--- ApplicationInsightsConsoleApp/Program.cs
+++ ApplicationInsightsConsoleApp/Program.cs
@@ -1,10 +1,76 @@
 ﻿namespace ApplicationInsightsConsoleApp
 {
-    internal class Program
+    #region using
+
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Hosting;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.Logging;
+
+    #endregion
+
+    public interface IMainRunner
     {
-        static void Main(string[] args)
+        public Task<int> RunAsync();
+    }
+
+    public class MainRunner : IMainRunner
+    {
+        private readonly ILogger<MainRunner> _logger;
+
+        public MainRunner(ILogger<MainRunner> logger)
         {
-            Console.WriteLine("Hello, World!");
+            _logger = logger;
+        }
+
+        public async Task<int> RunAsync()
+        {
+            await Task.Run(() =>
+            {
+                _logger.LogInformation("Hello from Main Runner!");
+            });
+
+            return 0;
+        }
+    }
+
+    public class Program
+    {
+        public static async Task<int> Main(string[] args)
+        {
+            var hostBuilder = CreateHostBuilder(args);
+
+            var host = hostBuilder.Build();
+
+            var taskResult = await host.Services.GetRequiredService<IMainRunner>().RunAsync();
+
+            host.Dispose();
+
+            return taskResult;
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .AddEnvironmentVariables()
+                .AddCommandLine(args)
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            return Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                {
+                    SetServices(services);
+                })
+                .ConfigureAppConfiguration( builder =>
+                {
+                    builder.AddConfiguration(configuration);
+                });
+        }
+
+        private static void SetServices(IServiceCollection services)
+        {
+            services.AddTransient<IMainRunner, MainRunner>();
         }
     }
 }
